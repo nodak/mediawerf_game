3c4402380c18cb822c25a9be6d6d69b4b7dbf1cd
Victor Diaz <victormdb@gmail.com>
2013-05-07 14:59:49 +0200

    remove osc dependencies

--- library_game/server.js
+++ library_game/server.js
@@ -3,14 +3,14 @@ var io = require('socket.io');
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
-var osc = require('node-osc'); 
+//var osc = require('node-osc'); 
 //var form = require('connect-form');
 
 
 var app = express();
 var server = http.createServer(app);
 server.listen(8080);
-var oscServer = new osc.Server(3333, '0.0.0.0');
+//var oscServer = new osc.Server(3333, '0.0.0.0');
 
 
 
@@ -111,26 +111,13 @@ websocket_server.sockets.on('connection', function (socket) {
 
 });
 
+/*
 oscServer.on("message", function (msg, rinfo) {
     
     console.log(msg);
     console.log(rinfo);
     websocket_server.sockets.emit("remoteController", msg); 
     
-    /*
-      var blobsJson=[]
-      //blobsJson.push( {'time':diffTime }) //timestamp
-      for (var i=2; i<msg.length; i+=1){
-        var blob=msg[i];  
-        if(blob[2]!=undefined){
-          if(blob[1]=='set'){
-            blobsJson.push({'x':Math.round(blob[3]*width) , 'y' :Math.round(blob[4]*height),'id':blob[2]} )                
-          }
-        }              
-      }            
-      //msgsArray.push(blobsJson)
-      //console.log( blobsJson )
-      */ 
-    // socket.broadcast.emit('updateClient', msg); 
 });
+*/
 
